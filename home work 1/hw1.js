a83f224408ea7172cc769720a0be3865c0d6eb84
janina-k <66694466+janina-k@users.noreply.github.com>
2024-05-30 21:17:10 +0200

    homework 1

---
+++ home work 1/hw1.js
@@ -0,0 +1,57 @@
+// task1
+
+// alert("Janina");
+
+// task2
+
+// console.log("Kwasny");
+
+// task3
+
+// let horse = "Galicja";
+// let rider = "Janina";
+
+// rider = horse;
+
+// console.log(horse);
+// console.log(rider);
+
+// task4
+
+// const mare = {
+//     name: "Galicja",
+//     age: 13,
+//     hadFoals: true,
+//     hadInjuries: undefined,
+//     hasUlcers: null
+// }
+
+// task5
+
+// let isAdult = confirm("Are you an adult?");
+
+// console.log(isAdult);
+
+// task7
+
+// let login = prompt("What's your login");
+// let email = prompt("What's your email address?");
+// let password = prompt("What's your password");
+
+// console.log(`Dear ${login}, your email is ${email}, your password is ${password}.`);
+
+// task8
+
+// const secondsInMinute = 60;
+// const minutesInHour = 60;
+// const secondsInHour = secondsInMinute * minutesInHour;
+
+// const hoursInDay = 24;
+// const secondsInDay = secondsInHour * hoursInDay;
+
+// const daysInMonth = 30;
+// const secondsInMonth = secondsInDay * daysInMonth;
+
+// console.log(`Seconds in an hour: ${secondsInHour}`);
+// console.log(`Seconds in a day: ${secondsInDay}`);
+// console.log(`Seconds in a month: ${secondsInMonth}`);
