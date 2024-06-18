169050721156e4be2e79f27506bcd59f6d354b3f
janina-k <66694466+janina-k@users.noreply.github.com>
2024-06-18 17:03:24 +0200

    homework 6

---
+++ home work 1/hw6.js
@@ -0,0 +1,33 @@
+// const test = document.getElementById("test");
+
+// console.log(test);
+
+// const test = document.getElementsByTagName("div");
+
+// console.log(test);
+
+// 
+
+// task 1
+
+// let test = document.querySelector("#test");
+
+// console.log(test);
+// test.innerHTML = "Last";
+
+// test.innerHTML = '<div>Last</div>';
+
+// task 2
+
+// let myImage = document.querySelector(".image");
+// myImage.src = 'dog.jpg';
+// alert(image.setAttribute(myImage));
+
+
+// task 3
+
+// let paragraphs = document.querySelectorAll("#text");
+// console.log(paragraphs);
+
+// task 4
+
